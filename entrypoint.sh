95f0d268abd3e5685db9847ba42c054e4016e2dd
Alessandro Chimetto <alessandro.chimetto.it@gmail.com>
2021-01-09 01:15:21 +0100

    Corretto script entrypoint.sh
    delatex deve eseguire nella cartella di main.tex causa \input che contengono ".." ovvero delatex considera il livello superiore della cartella in cui è stato eseguito e non dove si trova il file

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,15 +4,17 @@
 # e rilasciato sotto licenza Unlicense <http://unlicense.org/>
 
 mkdir -p $GITHUB_WORKSPACE/$2
-cd $GITHUB_WORKSPACE/$2
 
-while IFS= read -r path $1
+while IFS= read -r path
 do
     if [ -n "$path" ]
     then
         name=$(basename $path)
-        delatex $path/main.tex > ./$name.txt
-        python3 /gulpease.py ./$name.txt > ./$name.txt
+        cd $GITHUB_WORKSPACE/$path
+        delatex main.tex > $GITHUB_WORKSPACE/$2/$name.txt
+        cd $GITHUB_WORKSPACE/$2
+        python3 /gulpease.py $name.txt > $name-eval.txt
+        rm $name.txt
     fi
-done
+done < "$GITHUB_WORKSPACE/$1"
 
